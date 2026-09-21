333dca3f21d1fece60753fded9666c1b2d981df6
Александр <sanya.starikov.2021@mail.ru>
2026-09-21 13:24:55 +0300

    сохр и загрузка в файл

--- Starikov_Laba/Starikov_Laba/Starikov_Laba.cpp
+++ Starikov_Laba/Starikov_Laba/Starikov_Laba.cpp
@@ -3,9 +3,8 @@
 
 #include <iostream>
 #include <vector>
+#include <fstream>
 using namespace std;
-
-
 
 struct Pipe {
     string Pipe_name;
@@ -84,8 +83,8 @@ while (c.count_workshops <= 0) {
 }
 cout << "Enter the count of active workshops in CS\n";
 cin >> c.active_workskops;
-while (c.active_workskops <= 0 || c.active_workskops > c.count_workshops) {
-    if (c.active_workskops <= 0) {
+while (c.active_workskops < 0 || c.active_workskops > c.count_workshops) {
+    if (c.active_workskops < 0) {
         cout << "ERROR: count of active workshops must be > 0\n";
     }
     else {
@@ -113,17 +112,14 @@ void view_objects() {
         cout << "No added pipes\n";
     }
     else {
-        for (size_t i = 0;i < pipes.size(); i++) {
-            cout << "Pipe #" << i + 1 << "\n";
-            cout << "Name: " << pipes[i].Pipe_name << "\n";
-            cout << "Diametr: " << pipes[i].diametr << "\n";
-            cout << "Lenght: " << pipes[i].lenght << "\n";
-            if (pipes[i].status == 'P') {
-                cout << "Status: Passive\n";
-            }
-            else {
-                cout << "Status: Active\n";
-            }
+        cout << "Name: " << pipes[0].Pipe_name << "\n";
+        cout << "Diametr: " << pipes[0].diametr << "\n";
+        cout << "Lenght: " << pipes[0].lenght << "\n";
+        if (pipes[0].status == 'P') {
+            cout << "Status: Passive\n";
+        }
+        else {
+            cout << "Status: Active\n";
         }
     }
     cout << "Your CSs:\n\n";
@@ -131,34 +127,16 @@ void view_objects() {
         cout << "No added CSs\n";
     }
     else {
-        for (size_t i = 0;i < css.size(); i++) {
-            cout << "CS #" << i + 1 << "\n";
-            cout << "Name: " << css[i].CS_name << "\n";
-            cout << "Count workshops: " << css[i].count_workshops << "\n";
-            cout << "Active workshops: " << css[i].active_workskops << "\n";
-            cout << "Class: " << css[i].CS_class << "\n";
-        }
+        cout << "Name: " << css[0].CS_name << "\n";
+        cout << "Count workshops: " << css[0].count_workshops << "\n";
+        cout << "Active workshops: " << css[0].active_workskops << "\n";
+        cout << "Class: " << css[0].CS_class << "\n";
     }
 }
 
 void edit_pipe() {
     if (pipes.empty()) {
         cout << "No added pipes\n";
-        return;
-    }
-    cout << "Avalibale pipes: \n";
-    for (int i = 0;i < pipes.size();i++) {
-        cout << i + 1 << " - " << pipes[i].Pipe_name << "\n";
-    }
-    int num;
-    cout << "Choose the number of pipe to edit (0 - cancel)\n";
-    cin >> num;
-
-    while (num < 0 || num >(int)pipes.size()) {
-        cout << "error\n";
-        cin >> num;
-    }
-    if (num == 0) {
         return;
     }
     cout << "\nWhat do you want to edit?\n";
@@ -169,17 +147,138 @@ void edit_pipe() {
     cout << "0 - Cancel\n";
     int f;
     cin >> f;
-    int index = num - 1;
     if (f == 0) {
         return;
     }
     else if (f == 1) {
         cout << "new name\n";
-        cin >> pipes[index].Pipe_name;
+        cin >> pipes[0].Pipe_name;
+    }
+    else if (f == 2) {
+        cout << "new diametr\n";
+        cin >> pipes[0].diametr;
+        while (pipes[0].diametr <= 0) {
+            cout << "ERROR: diametr must be positive! Enter again\n";
+            cin >> pipes[0].diametr;
+        }
+    }
+    else if (f == 3) {
+        cout << "new lenght\n";
+        cin >> pipes[0].lenght;
+        while (pipes[0].lenght <= 0) {
+            cout << "ERROR: lenght must be positive! Enter again\n";
+            cin >> pipes[0].lenght;
+        }
+    }
+    else if (f == 4) {
+        cout << "new status (P - passive, A - active)\n";
+        cin >> pipes[0].status;
+        while (pipes[0].status != 'P' && pipes[0].status != 'A') {
+            cout << "status of pipe must be 'P' or 'A'\n";
+            cin >> pipes[0].status;
+        }
     }
     else {
         cout << "error\n";
     }
+}
+
+void edit_CS() {
+    if (css.empty()) {
+        cout << "No added CS\n";
+        return;
+    }
+    cout << "\nWhat do you want to edit?\n";
+    cout << "1 - Name\n";
+    cout << "2 - count workshops\n";
+    cout << "3 - active workshops\n";
+    cout << "4 - Class\n";
+    cout << "0 - Cancel\n";
+    int f;
+    cin >> f;
+    if (f == 0) {
+        return;
+    }
+    else if (f == 1) {
+        cout << "new name\n";
+        cin >> css[0].CS_name;
+    }
+    else if (f == 2) {
+        cout << "new count workshops\n";
+        cin >> css[0].count_workshops;
+        while (css[0].count_workshops <= 0) {
+            cout << "error\n";
+            cin >> css[0].count_workshops;
+        }
+    }
+    else if (f == 3) {
+        cout << "new count active workshops\n";
+        cin >> css[0].active_workskops;
+        while (css[0].active_workskops > css[0].count_workshops || css[0].active_workskops < 0) {
+            if (css[0].active_workskops < 0) {
+                cout << "error\n";
+                cin >> css[0].active_workskops;
+            }
+            else {
+                cout << "error\n";
+                cin >> css[0].active_workskops;
+            }
+        }
+    }
+    else if (f == 4) {
+        cout << "new class\n";
+        cin >> css[0].CS_class;
+        while (css[0].CS_class != 'A' && css[0].CS_class != 'B' && css[0].CS_class != 'C' && css[0].CS_class != 'D') {
+            cout << "error (A,B,C,D)\n";
+            cin >> css[0].CS_class;
+        }
+    }
+    else {
+        cout << "error\n";
+    }
+}
+
+void save() {
+    ofstream out("save.txt");
+    if (!out.is_open()) {
+        cout << "error\n";
+        return;
+    }
+    if (!pipes.empty()) {
+        out << pipes[0].Pipe_name << "\n";
+        out << pipes[0].diametr << "\n";
+        out << pipes[0].lenght << "\n";
+        out << pipes[0].status << "\n";
+    }
+    if (!css.empty()) {
+        out << css[0].CS_name << "\n";
+        out << css[0].count_workshops << "\n";
+        out << css[0].active_workskops << "\n";
+        out << css[0].CS_class << "\n";
+    }
+    out.close();
+    cout << "data was saving\n";
+}
+
+void download() {
+    ifstream in("save.txt");
+    if (!in.is_open()) {
+        cout << "error\n";
+        return;
+    }
+    pipes.clear();
+    css.clear();
+    Pipe p;
+    if (in >> p.Pipe_name >> p.diametr >> p.lenght >> p.status) {
+        pipes.push_back(p);
+    }
+
+    CS c;
+    if (in >> c.CS_name >> c.count_workshops >> c.active_workskops >> c.CS_class) {
+        css.push_back(c);
+    }
+    in.close();
+    cout << "data was downloading\n";
 }
 
 int main()
@@ -208,6 +307,18 @@ int main()
 
                 else if (user == 4) {
                     edit_pipe();
+                }
+
+                else if (user == 5) {
+                    edit_CS();
+                }
+
+                else if (user == 6) {
+                    save();
+                }
+
+                else if (user == 7) {
+                    download();
                 }
 
                 else if (user == 0) {
