f5f9dc1db6034520d0aaf5e09dc9494cb89bfc88
Александр <sanya.starikov.2021@mail.ru>
2026-09-21 15:24:14 +0300

    проверки

--- Starikov_Laba/Starikov_Laba/Starikov_Laba.cpp
+++ Starikov_Laba/Starikov_Laba/Starikov_Laba.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <limits>
 using namespace std;
 
 struct Pipe {
@@ -36,19 +37,26 @@ void zapusk() {
     cout << "0 - Exit\n";
 }
 
+void check() {
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+}
+
 void add_pipe() {
     Pipe p;
     cout << "Give a name to the pipe.\n";
     cin >> p.Pipe_name;
     cout << "Enter the pipe diameter.\n";
     cin >> p.diametr;
-    while (p.diametr <= 0) {
+    while (p.diametr <= 0 || cin.fail()) {
+        check();
         cout << "ERROR: diametr must be positive! Enter again\n";
         cin >> p.diametr;
     }
     cout << "Enter the length of the pipe.\n";
     cin >> p.lenght;
-    while (p.lenght <= 0) {
+    while (p.lenght <= 0 || cin.fail()) {
+        check();
         cout << "ERROR: lenght must be positive! Enter again\n";
         cin >> p.lenght;
     }
@@ -59,7 +67,7 @@ void add_pipe() {
         cin >> p.status;
     }
     pipes.push_back(p);
-    cout << "You add a new pipe!\n\n";
+    cout << "\nYou add a new pipe!\n\n";
     cout << "Pipe name: " << p.Pipe_name << "\n";
     cout << "Diametr: " << p.diametr << "\n";
     cout << "Lenght: " << p.lenght << "\n";
@@ -77,15 +85,19 @@ cout << "Give a name to the CS\n";
 cin >> c.CS_name;
 cout << "Enter the count of workshops in CS\n";
 cin >> c.count_workshops;
-while (c.count_workshops <= 0) {
+while (c.count_workshops <= 0 || cin.fail()) {
+    check();
     cout << "ERROR: count of workshops must be positive\n";
     cin >> c.count_workshops;
 }
 cout << "Enter the count of active workshops in CS\n";
 cin >> c.active_workskops;
-while (c.active_workskops < 0 || c.active_workskops > c.count_workshops) {
+while (c.active_workskops < 0 || c.active_workskops > c.count_workshops || cin.fail()) {
     if (c.active_workskops < 0) {
         cout << "ERROR: count of active workshops must be > 0\n";
+    }
+    else if (cin.fail()) {
+        check();
     }
     else {
         cout << "ERROR: total active workshops must be < count of workshops\n";
@@ -139,47 +151,12 @@ void edit_pipe() {
         cout << "No added pipes\n";
         return;
     }
-    cout << "\nWhat do you want to edit?\n";
-    cout << "1 - Name\n";
-    cout << "2 - Diameter\n";
-    cout << "3 - Length\n";
-    cout << "4 - Status\n";
-    cout << "0 - Cancel\n";
-    int f;
-    cin >> f;
-    if (f == 0) {
-        return;
-    }
-    else if (f == 1) {
-        cout << "new name\n";
-        cin >> pipes[0].Pipe_name;
-    }
-    else if (f == 2) {
-        cout << "new diametr\n";
-        cin >> pipes[0].diametr;
-        while (pipes[0].diametr <= 0) {
-            cout << "ERROR: diametr must be positive! Enter again\n";
-            cin >> pipes[0].diametr;
-        }
-    }
-    else if (f == 3) {
-        cout << "new lenght\n";
-        cin >> pipes[0].lenght;
-        while (pipes[0].lenght <= 0) {
-            cout << "ERROR: lenght must be positive! Enter again\n";
-            cin >> pipes[0].lenght;
-        }
-    }
-    else if (f == 4) {
-        cout << "new status (P - passive, A - active)\n";
+    cout << "You may change pipe status(P - passive, A - active)\n";
+    cout << "Status: " << pipes[0].status << "\n";
+    cin >> pipes[0].status;
+    while (pipes[0].status != 'P' && pipes[0].status != 'A') {
+        cout << "ERROR: status of pipe must be 'P' or 'A'\n";
         cin >> pipes[0].status;
-        while (pipes[0].status != 'P' && pipes[0].status != 'A') {
-            cout << "status of pipe must be 'P' or 'A'\n";
-            cin >> pipes[0].status;
-        }
-    }
-    else {
-        cout << "error\n";
     }
 }
 
@@ -188,53 +165,19 @@ void edit_CS() {
         cout << "No added CS\n";
         return;
     }
-    cout << "\nWhat do you want to edit?\n";
-    cout << "1 - Name\n";
-    cout << "2 - count workshops\n";
-    cout << "3 - active workshops\n";
-    cout << "4 - Class\n";
-    cout << "0 - Cancel\n";
-    int f;
-    cin >> f;
-    if (f == 0) {
-        return;
-    }
-    else if (f == 1) {
-        cout << "new name\n";
-        cin >> css[0].CS_name;
-    }
-    else if (f == 2) {
-        cout << "new count workshops\n";
-        cin >> css[0].count_workshops;
-        while (css[0].count_workshops <= 0) {
+    cout << "You may change count of active workshops\n";
+    cout << "Active workshops: " << css[0].active_workskops << "\n";
+    cin >> css[0].active_workskops;
+    while ((css[0].active_workskops > css[0].count_workshops) || cin.fail()) {
+        if (cin.fail()) {
+            check();
             cout << "error\n";
-            cin >> css[0].count_workshops;
+            cin >> css[0].active_workskops;
         }
-    }
-    else if (f == 3) {
-        cout << "new count active workshops\n";
-        cin >> css[0].active_workskops;
-        while (css[0].active_workskops > css[0].count_workshops || css[0].active_workskops < 0) {
-            if (css[0].active_workskops < 0) {
-                cout << "error\n";
-                cin >> css[0].active_workskops;
-            }
-            else {
-                cout << "error\n";
-                cin >> css[0].active_workskops;
-            }
+        else {
+            cout << "error\n";
+            cin >> css[0].active_workskops;
         }
-    }
-    else if (f == 4) {
-        cout << "new class\n";
-        cin >> css[0].CS_class;
-        while (css[0].CS_class != 'A' && css[0].CS_class != 'B' && css[0].CS_class != 'C' && css[0].CS_class != 'D') {
-            cout << "error (A,B,C,D)\n";
-            cin >> css[0].CS_class;
-        }
-    }
-    else {
-        cout << "error\n";
     }
 }
 
@@ -245,7 +188,7 @@ void save() {
         return;
     }
     if (!pipes.empty()) {
-        out << pipes[0].Pipe_name << "\n";
+        out << "Name: " << pipes[0].Pipe_name << "\n";
         out << pipes[0].diametr << "\n";
         out << pipes[0].lenght << "\n";
         out << pipes[0].status << "\n";
@@ -261,7 +204,7 @@ void save() {
 }
 
 void download() {
-    ifstream in("save.txt");
+    ifstream in("download.txt");
     if (!in.is_open()) {
         cout << "error\n";
         return;
@@ -336,14 +279,3 @@ int main()
         }
     }
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
